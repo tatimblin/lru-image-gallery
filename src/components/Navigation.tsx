a3729fc6be09c574e40a1f9a0ef8b10667dd2c1c
Tristan Timblin <tatimblin@gmail.com>
2023-11-12 20:47:11 -0800

    navigation ui

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,15 +1,37 @@
 import Link from "next/link";
+import Image from "next/image";
+
+type NavigationLink = {
+	label: string,
+	icon: string,
+	route: string,
+};
 
 export function Navigation() {
+	const links: NavigationLink[] = [
+		{
+			label: "Recents",
+			icon: "/recent.svg",
+			route: "/recents",
+		},
+		{
+			label: "All",
+			icon: "/album.svg",
+			route: "/",
+		},
+	];
+
 	return (
 		<nav className="fixed w-full md:w-auto md:h-screen bottom-0 p-4">
 			<ul className="flex justify-center gap-4 md:flex-col md:h-full">
-				<li>
-					<Link href="/recents">Recents</Link>
-				</li>
-				<li>
-					<Link href="/">All</Link>
-				</li>
+				{links.map((link) => (
+					<li>
+						<Link href={link.route} className="group flex items-center gap-2 w-8">
+							<Image src={link.icon} alt="" width="32" height="32" className="opacity-30 hover:opacity-100" />
+							<span className="text-xs text-gray-400 opacity-0 group-hover:opacity-100 pointer-events-none">{link.label}</span>
+						</Link>
+					</li>
+				))}
 			</ul>
 		</nav>
 	);
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,30 @@
+"use client"
+
+import { useState, SyntheticEvent } from "react";
+import Image from "next/image";
+
 export function SearchBar() {
+    const [suggestions, setSuggestions] = useState(["one", "two", "three"]);
+
+    const handleSubmit = (event: SyntheticEvent) => {
+        event.preventDefault();
+    }
+
     return (
-        <div>
-            SearchBar
+        <div className="relative">
+            <form onSubmit={handleSubmit} className="relative z-10 flex align-middle bg-slate-100 rounded-xl">
+                <input type="text" placeholder="Search" className="py-2 px-4 bg-transparent focus:outline-none" />
+                <button type="submit" className="pr-4 opacity-50 hover:opacity-100">
+                    <Image src="/search.svg" alt="Search" width="22" height="22" />
+                </button>
+            </form>
+            <ul className="absolute w-full bg-slate-100 rounded-b-xl top-1/2 pt-4">
+                {suggestions.map((suggestion) => (
+                    <li className="px-4 py-2">
+                        {suggestion}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
