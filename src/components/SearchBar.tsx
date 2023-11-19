526d21a7b172137cbd662ce11b74c654265ee171
Tristan Timblin <tatimblin@gmail.com>
2023-11-18 17:01:24 -0800

    animate autocomplete

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,7 +8,7 @@ import useMountTransition from "../hooks/useMountTransition";
 export function SearchBar() {
     const [suggestions, setSuggestions] = useState(["one", "two", "three"]);
     const [reveal, setReveal] = useState(false);
-    const hasTransitionedIn = useMountTransition(reveal, 1000);
+    const hasTransitionedIn = useMountTransition(reveal, 200);
 
     const handleSubmit = (event: SyntheticEvent) => {
         event.preventDefault();
@@ -20,7 +20,7 @@ export function SearchBar() {
 
     return (
         <div className="relative w-full sm:w-fit">
-            <form onSubmit={handleSubmit} className="relative z-10 flex align-middle bg-slate-100 hover:bg-slate-200 focus-within:bg-slate-200 sm:rounded-xl sm:focus-within:rounded-b-none">
+            <form onSubmit={handleSubmit} className="relative z-10 flex align-middle bg-slate-100 hover:bg-slate-200 focus-within:bg-slate-200 sm:rounded-xl sm:focus-within:rounded-b-none ease-in duration-100">
                 <input
                     onFocus={handleFocus}
                     onBlur={handleFocus}
@@ -32,9 +32,12 @@ export function SearchBar() {
                     <Image src="/search.svg" alt="Search" width="32" height="32" />
                 </button>
             </form>
-            {(hasTransitionedIn || reveal) && (
+            {(reveal || hasTransitionedIn) && (
                 <ul className={clsx(
-                    "absolute w-full bg-slate-100 sm:rounded-b-xl top-0 pt-4 ease-in duration-300"
+                    "absolute w-full bg-slate-100 sm:rounded-b-xl -translate-y-2 opacity-0 ease-in duration-200",
+                    {
+                        "-translate-y-0 opacity-100": (reveal && hasTransitionedIn),
+                    },
                 )}>
                     {suggestions.map((suggestion) => (
                         <li key={suggestion} className="px-4 py-2">
